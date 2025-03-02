05b895b18fcefd9ec3ea18b0c764e010bf117158
daria <daryaagru@gmail.com>
2025-03-03 00:27:39 +0700

    Updates

--- src/components/Links.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <router-link to="/about">О нас</router-link>
-    <p>Популярное</p>
-    <router-link to="/travels">Туры</router-link>
-    <p>Отзывы</p>
-    <router-link to="/contacts">Контакты</router-link>
-</template>
